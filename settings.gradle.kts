4deefcceea9aa42c6d71329457dea1ccfe80cb13
Nek-12 <vaizin.nikita@gmail.com>
2023-01-29 21:37:57 +0300

    try workaround of CI resolution of K/N

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,32 @@ dependencyResolutionManagement {
         mavenCentral()
         ivy { url = uri("https://download.jetbrains.com") }
         maven { url = uri("https://jitpack.io") }
+
+        exclusiveContent {
+            forRepository {
+                ivy("https://download.jetbrains.com/kotlin/native/builds") {
+                    name = "Kotlin Native"
+                    patternLayout {
+
+                        listOf(
+                            "macos-x86_64",
+                            "macos-aarch64",
+                            "osx-x86_64",
+                            "osx-aarch64",
+                            "linux-x86_64",
+                            "windows-x86_64",
+                        ).forEach { os ->
+                            listOf("dev", "releases").forEach { stage ->
+                                artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
+                            }
+                        }
+                    }
+                    metadataSources { artifact() }
+                }
+            }
+            filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
+        }
+
     }
 }
 
