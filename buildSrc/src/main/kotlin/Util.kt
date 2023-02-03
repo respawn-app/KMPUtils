c9e64b7b60fe036915d6f774bdc7e35085651963
Nek-12 <vaizin.nikita@gmail.com>
2023-02-03 11:26:41 +0300

    some tweaks to build processes

--- buildSrc/src/main/kotlin/Util.kt
+++ buildSrc/src/main/kotlin/Util.kt
@@ -59,35 +59,3 @@ fun List<String>.toJavaArrayString() = buildString {
 }
 
 fun String.toBase64() = Base64.getEncoder().encodeToString(toByteArray())
-
-/**
- *  workaround CI pipeline resolution bug
- */
-fun RepositoryHandler.ivy() {
-    ivy { url = URI("https://download.jetbrains.com") }
-
-    exclusiveContent {
-        forRepository {
-            this@ivyNative.ivy("https://download.jetbrains.com/kotlin/native/builds") {
-                name = "Kotlin Native"
-                patternLayout {
-
-                    listOf(
-                        "macos-x86_64",
-                        "macos-aarch64",
-                        "osx-x86_64",
-                        "osx-aarch64",
-                        "linux-x86_64",
-                        "windows-x86_64",
-                    ).forEach { os ->
-                        listOf("dev", "releases").forEach { stage ->
-                            artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
-                        }
-                    }
-                }
-                metadataSources { artifact() }
-            }
-        }
-        filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
-    }
-}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -20,7 +20,7 @@ dependencyResolutionManagement {
 
     repositories {
         google()
-        ivy()
+        ivyNative()
         mavenCentral()
     }
 }
@@ -31,3 +31,36 @@ include(":apiresult")
 include(":common")
 include(":datetime")
 include(":coroutines")
+
+
+/**
+ *  workaround CI pipeline resolution bug
+ */
+fun RepositoryHandler.ivyNative() {
+    ivy { url = java.net.URI("https://download.jetbrains.com") }
+
+    exclusiveContent {
+        forRepository {
+            this@ivyNative.ivy("https://download.jetbrains.com/kotlin/native/builds") {
+                name = "Kotlin Native"
+                patternLayout {
+
+                    listOf(
+                        "macos-x86_64",
+                        "macos-aarch64",
+                        "osx-x86_64",
+                        "osx-aarch64",
+                        "linux-x86_64",
+                        "windows-x86_64",
+                    ).forEach { os ->
+                        listOf("dev", "releases").forEach { stage ->
+                            artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
+                        }
+                    }
+                }
+                metadataSources { artifact() }
+            }
+        }
+        filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
+    }
+}
