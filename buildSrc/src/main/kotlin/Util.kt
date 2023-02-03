ef383a83d99aec3d5267f015da41da3e3f54f322
Nek-12 <vaizin.nikita@gmail.com>
2023-02-03 10:03:19 +0300

    add ivyNative to repo management

--- buildSrc/src/main/kotlin/Util.kt
+++ buildSrc/src/main/kotlin/Util.kt
@@ -8,8 +8,11 @@
 import org.gradle.api.Project
 import org.gradle.api.artifacts.VersionCatalog
 import org.gradle.api.artifacts.VersionCatalogsExtension
+import org.gradle.api.artifacts.dsl.RepositoryHandler
 import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.ivy
 import org.gradle.plugin.use.PluginDependency
+import java.net.URI
 import java.util.Base64
 
 /**
@@ -56,3 +59,35 @@ fun List<String>.toJavaArrayString() = buildString {
 }
 
 fun String.toBase64() = Base64.getEncoder().encodeToString(toByteArray())
+
+/**
+ *  workaround CI pipeline resolution bug
+ */
+fun RepositoryHandler.ivy() {
+    ivy { url = URI("https://download.jetbrains.com") }
+
+    exclusiveContent {
+        forRepository {
+            this@ivyNative.ivy("https://download.jetbrains.com/kotlin/native/builds") {
+                name = "Kotlin Native"
+                patternLayout {
+
+                    listOf(
+                        "macos-x86_64",
+                        "macos-aarch64",
+                        "osx-x86_64",
+                        "osx-aarch64",
+                        "linux-x86_64",
+                        "windows-x86_64",
+                    ).forEach { os ->
+                        listOf("dev", "releases").forEach { stage ->
+                            artifact("$stage/[revision]/$os/[artifact]-[revision].[ext]")
+                        }
+                    }
+                }
+                metadataSources { artifact() }
+            }
+        }
+        filter { includeModuleByRegex(".*", ".*kotlin-native-prebuilt.*") }
+    }
+}
