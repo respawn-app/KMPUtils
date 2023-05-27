5066538c5d2fa65886d089e8ac2f7d827a2c87ed
Nek-12 <vaizin.nikita@gmail.com>
2023-05-27 13:21:56 +0300

    add module for inputforms

---
+++ inputforms/build.gradle.kts
@@ -0,0 +1,20 @@
+@Suppress("DSL_SCOPE_VIOLATION")
+plugins {
+    id("pro.respawn.shared-library")
+}
+
+kotlin {
+    configureMultiplatform(
+        this,
+        android = false,
+        ios = true,
+        jvm = true,
+        js = true,
+        linux = true,
+        mingw = true,
+    )
+}
+
+dependencies {
+    commonMainImplementation(libs.arrow)
+}
