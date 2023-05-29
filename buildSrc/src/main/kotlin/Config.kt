91f70b5a77688c03dcc0a90e7e7f9019d361f50f
Nek-12 <vaizin.nikita@gmail.com>
2023-05-29 18:07:47 +0300

    set version to alpha

--- buildSrc/src/main/kotlin/Config.kt
+++ buildSrc/src/main/kotlin/Config.kt
@@ -18,7 +18,7 @@ object Config {
     const val majorRelease = 1
     const val minorRelease = 1
     const val patch = 0
-    const val versionName = "$majorRelease.$minorRelease.$patch"
+    const val versionName = "$majorRelease.$minorRelease.$patch-alpha01"
 
     // kotlin
 
