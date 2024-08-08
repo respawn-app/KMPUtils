b0c31cf3815a5dbf34fa0b570f84d99234dcb888
Nek-12 <vaizin.nikita@gmail.com>
2024-08-08 14:12:41 +0200

    add docs

--- compose/src/commonMain/kotlin/pro/respawn/kmmutils/compose/modifier/Clickable.kt
+++ compose/src/commonMain/kotlin/pro/respawn/kmmutils/compose/modifier/Clickable.kt
@@ -1,16 +1,21 @@
 package pro.respawn.kmmutils.compose.modifier
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.semantics.Role
 
+/**
+ * Add a clickable modifier that has no indication (no ripple)
+ */
 public fun Modifier.noIndicationClickable(
     enabled: Boolean = true,
     onClickLabel: String? = null,
     role: Role? = null,
+    interactionSource: MutableInteractionSource? = null,
     onClick: () -> Unit,
 ): Modifier = clickable(
-    interactionSource = null,
+    interactionSource = interactionSource,
     indication = null,
     enabled = enabled,
     onClickLabel = onClickLabel,
