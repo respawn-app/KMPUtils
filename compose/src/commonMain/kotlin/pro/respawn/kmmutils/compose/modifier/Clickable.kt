26e47e0ccc606fbbd2784ac89599fe2735f916f3
Nek-12 <vaizin.nikita@gmail.com>
2024-08-08 14:10:23 +0200

    feat: add noIndicationClickable and font() functions to compose

--- compose/src/commonMain/kotlin/pro/respawn/kmmutils/compose/modifier/Clickable.kt
+++ compose/src/commonMain/kotlin/pro/respawn/kmmutils/compose/modifier/Clickable.kt
@@ -1,17 +1,19 @@
 package pro.respawn.kmmutils.compose.modifier
 
-// TODO: Waiting for compose update
+import androidx.compose.foundation.clickable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.semantics.Role
 
-// public fun Modifier.noIndicationClickable(
-//     enabled: Boolean = true,
-//     onClickLabel: String? = null,
-//     role: Role? = null,
-//     onClick: () -> Unit,
-// ): Modifier = clickable(
-//     interactionSource = null,
-//     indication = null,
-//     enabled = enabled,
-//     onClickLabel = onClickLabel,
-//     role = role,
-//     onClick = onClick
-// )
+public fun Modifier.noIndicationClickable(
+    enabled: Boolean = true,
+    onClickLabel: String? = null,
+    role: Role? = null,
+    onClick: () -> Unit,
+): Modifier = clickable(
+    interactionSource = null,
+    indication = null,
+    enabled = enabled,
+    onClickLabel = onClickLabel,
+    role = role,
+    onClick = onClick
+)
