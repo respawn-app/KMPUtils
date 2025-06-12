eef66cafa8d74466ae374e8adb612e8848b0e4c7
Nek-12 <vaizin.nikita@gmail.com>
2025-06-12 10:20:55 +0200

    fix: allow dot symbol in email validation

--- inputforms/src/commonMain/kotlin/pro/respawn/kmmutils/inputforms/default/Patterns.kt
+++ inputforms/src/commonMain/kotlin/pro/respawn/kmmutils/inputforms/default/Patterns.kt
@@ -1,5 +1,8 @@
 package pro.respawn.kmmutils.inputforms.default
 
+import pro.respawn.kmmutils.inputforms.default.Patterns.InternationalDelimitedPhoneNumber
+import pro.respawn.kmmutils.inputforms.default.Patterns.InternationalNumericPhoneNumber
+
 /**
  * Common regular expressions used for form validation
  */
@@ -34,7 +37,7 @@ public data object Patterns {
      * - Does not limit the length of the input. Use other validations to limit the length according to your needs.
      */
     public val Email: Regex by lazy {
-        """(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])""".toRegex()
+        """^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")\.*@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)])$""".toRegex()
     }
 
     /**
