43dd9c79c811ad8395590a13d82546cf93af3521
Nek-12 <vaizin.nikita@gmail.com>
2025-06-30 12:49:05 +0200

    fix: no longer force throw from coroutine receiver, disallow overriding score

--- system/src/androidMain/kotlin/pro/respawn/kmmutils/system/android/CoroutineReceiver.kt
+++ system/src/androidMain/kotlin/pro/respawn/kmmutils/system/android/CoroutineReceiver.kt
@@ -6,21 +6,20 @@ import android.content.Intent
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.SupervisorJob
-import kotlinx.coroutines.TimeoutCancellationException
 import kotlinx.coroutines.launch
-import kotlinx.coroutines.withTimeout
+import kotlinx.coroutines.withTimeoutOrNull
 import kotlin.time.Duration.Companion.seconds
 
 /**
  * A [BroadcastReceiver] that calls [goAsync] and launches a coroutine to execute long-running tasks.
  * The [receive] method is still executed on main thread, move to another thread as needed.
  *
- * According to Android limitations, you still have about **10 seconds** to finish the execution before
+ * According to Android limitations, you have about **10 seconds** to finish the execution before
  * the system forcibly kills the receiver. For longer tasks, use WorkManager or a Service
  */
 public abstract class CoroutineReceiver : BroadcastReceiver() {
 
-    protected val scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
+    private val scope: CoroutineScope = CoroutineScope(SupervisorJob() + Dispatchers.Main.immediate)
 
     override fun onReceive(context: Context, intent: Intent?) {
         if (intent == null) return
@@ -28,9 +27,9 @@ public abstract class CoroutineReceiver : BroadcastReceiver() {
         val result = goAsync()
         scope.launch {
             try {
-                withTimeout(MaxDuration) { receive(context, intent) }
-            } catch (e: TimeoutCancellationException) {
-                throw IllegalStateException(Message, e)
+                withTimeoutOrNull(MaxDuration) {
+                    receive(context, intent)
+                } ?: onTimeout(IllegalStateException(Message))
             } finally {
                 result.finish()
             }
@@ -43,12 +42,14 @@ public abstract class CoroutineReceiver : BroadcastReceiver() {
      */
     protected abstract suspend fun receive(context: Context, intent: Intent)
 
+    protected open fun onTimeout(exception: Exception): Unit = throw exception
+
     private companion object {
 
         private val MaxDuration = 10.seconds
         private val Message = """
-            CoroutineReceiver has been suspended for more than ${MaxDuration.inWholeSeconds}
-            You cannot execute CoroutineReceiver for a long time because the system will kill it.
+            CoroutineReceiver has been suspended for more than $MaxDuration
+            You cannot execute code in broadcast receivers for longer periods of time due to OS constraints. 
             If you want to execute long-running tasks, launch a worker from your receiver.
         """.trimIndent()
     }
